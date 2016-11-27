395fd6c760e91e9589098c433992efcc96e91a2a
macsj200 <maxjohansen@berkeley.edu>
2016-11-27 11:47:36 -0800

    WIP basic support

--- autoform-images.js
+++ autoform-images.js
@@ -1,15 +1,36 @@
 import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+import { Meteor } from 'meteor/meteor';
+
+const imageFile = new ReactiveVar();
+const imageURL = new ReactiveVar("");
 
 const uploader = new Slingshot.Upload("myFileUploads");
 
 AutoForm.addInputType('afImage', {
   template:'addImageTemplate',
+  valueOut(){
+    console.log(imageURL.get());
+    return imageURL.get();
+  },
 });
+
+deleteImage = () => {
+  if(imageURL.get() !== ""){
+    // TODO complete
+    // https://github.com/CulturalMe/meteor-slingshot/issues/50#issuecomment-80965314
+  }
+}
+
+Template.addImageTemplate.destroyed = function () {
+  deleteImage();
+};
 
 Template.addImageTemplate.events({
   'change .image-file-button'(event, target){
-    const file = event.currentTarget.files[0];
-    uploader.send(file, function (error, downloadUrl) {
+    deleteImage();
+    imageFile.set(event.currentTarget.files[0]);
+    uploader.send(imageFile.get(), function (error, downloadUrl) {
       if (error) {
         console.log(error);
         // Log service detailed response.
@@ -17,7 +38,7 @@ Template.addImageTemplate.events({
         // alert (error);
       }
       else {
-        console.log(downloadUrl);
+        imageURL.set(downloadUrl);
         // Meteor.users.update(Meteor.userId(), {$push: {"profile.files": downloadUrl}});
       }
     });
