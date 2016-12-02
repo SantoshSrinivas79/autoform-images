f680b04789ca63b9f0012b519233f44cb43d6815
macsj200 <maxjohansen@berkeley.edu>
2016-12-01 19:42:03 -0800

    bump version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'maxjohansen:autoform-images',
-  version: '0.0.3',
+  version: '0.0.4',
   // Brief, one-line summary of the package.
   summary: 'Image upload for Autoform',
   // URL to the Git repository containing the source code for this package.
