f20f16207d4b66811aa95fb0b0866c59b0452f4c
Wesley Smith <wesleyfsmith@gmail.com>
2016-12-13 14:38:24 -0700

    Tries to fix array url issue, WIP

--- autoform-images.js
+++ autoform-images.js
@@ -2,48 +2,75 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 import { Meteor } from 'meteor/meteor';
 
-let fileUrl = new ReactiveVar("");
+export const fileUrlMap = {};
+
+const interation = 0;
 
 AutoForm.addInputType('afImageElem', {
   template:'addImageElemTemplate',
   valueOut(){
-    return fileUrl.get();
-  },
+    console.log(AutoForm.getCurrentDataForForm());
+    if (!fileUrlMap[this.context.id]) {
+      console.log('failing on id: ' + this.context.id);
+      console.log(fileUrlMap);
+      console.log(this);
+      console.log(this.val());
+      return '';
+    } else {
+      return fileUrlMap[this.context.id].get();
+    }
+  }
 });
+
 
 Template.addImageElemTemplate.onCreated(function(){
   this.uploader = new Slingshot.Upload("myFileUploads");
+
+  fileUrlMap[this.data.atts.id] = new ReactiveVar(Template.instance().data.value || '');
+
+  this.fileUrl = () => {
+    return fileUrlMap[this.data.atts.id];
+  }
 });
 
+// Template.addImageElemTemplate.onRendered(function() {
+//
+//
+//   $('#' + this.data.atts.id).context.fileUrl = new ReactiveVar('');
+// });
+
 Template.addImageElemTemplate.onDestroyed(function(){
-  if(fileUrl.get() !== "") {
+  // if(Template.instance().fileUrl().get() !== "") {
     // TODO fix image cleanup
     // const deletePath = fileUrl.get().slice(fileUrl.get().indexOf('.com') + '.com'.length)
     // S3.delete(deletePath, (err,res) => {
     //   console.log(err,res);
     // });
-  }
+  // }
 });
 
 Template.addImageElemTemplate.events({
   'change .image-file-button'(event, target){
+
+    const templateInstance = Template.instance();
+
     if(event.target.files.length !== 0){
-      Template.instance().uploader.send(event.target.files[0], function (error, downloadUrl) {
+      templateInstance.uploader.send(event.target.files[0], function (error, downloadUrl) {
         if (error) {
           console.log(error);
-          fileUrl.set("");
+          templateInstance.fileUrl().set("");
           // Log service detailed response.
           // console.error('Error uploading', uploader.xhr.response);
           // alert (error);
         }
         else {
-          fileUrl.set(downloadUrl);
+          templateInstance.fileUrl().set(downloadUrl);
           // imageURL.set(downloadUrl);
           // Meteor.users.update(Meteor.userId(), {$push: {"profile.files": downloadUrl}});
         }
       });
     } else {
-      fileUrl.set("");
+      templateInstance.fileUrl().set("");
     }
   }
 });
@@ -61,4 +88,7 @@ Template.addImageElemTemplate.helpers({
   shouldShowProgress: function(){
     return !isNaN(Template.instance().uploader.progress());
   },
+  getAtts() {
+    return this.atts;
+  }
 });
--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'maxjohansen:autoform-images',
-  version: '0.0.10',
+  version: '0.0.12',
   // Brief, one-line summary of the package.
   summary: 'Image upload for Autoform',
   // URL to the Git repository containing the source code for this package.
@@ -17,13 +17,15 @@ Package.onUse(function(api) {
   api.use('edgee:slingshot@0.7.1');
   api.use('aldeed:autoform@5.8.1');
   api.use('lepozepo:s3@5.2.4');
-  // api.mainModule('autoform-images.js');
   // api.addAssets(['addImageTemplate.html'], 'client');
   api.addFiles([
-    'addImageTemplate.html','autoform-images.js',
+    'addImageTemplate.html',
   ], 'client');
   api.addFiles(['SlingshotConfig.js','S3Methods.js'],['client','server']);
   api.addFiles(['SlingshotDirective.js','S3Config.js'],['server']);
+
+  api.mainModule('autoform-images.js', 'client');
+
 });
 
 Package.onTest(function(api) {
