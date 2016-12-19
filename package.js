22406363f36eb42128c52e014ff7f3e97607fc85
Wesley Smith <wesleyfsmith@gmail.com>
2016-12-18 18:58:00 -0700

    Bumps version number

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'maxjohansen:autoform-images',
-  version: '0.0.12',
+  version: '0.0.13',
   // Brief, one-line summary of the package.
   summary: 'Image upload for Autoform',
   // URL to the Git repository containing the source code for this package.
