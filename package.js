9c7c54e8e5a4f6afc3a6d5093e6d26008dcedb20
macsj200 <maxjohansen@berkeley.edu>
2017-03-09 12:30:43 -0800

    bump version

--- package.js
+++ package.js
@@ -1,36 +1,37 @@
+Npm.depends({
+  'phantomjs-prebuilt':'2.1.14',
+});
 Package.describe({
   name: 'maxjohansen:autoform-images',
-  version: '0.0.14',
+  version: '0.0.15',
   // Brief, one-line summary of the package.
   summary: 'Image upload for Autoform',
   // URL to the Git repository containing the source code for this package.
   git: '',
   // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.4.2.1');
+  api.versionsFrom('1.4.3.1');
   api.use('ecmascript');
   api.use('templating@1.2.15');
   api.use('edgee:slingshot@0.7.1');
   api.use('aldeed:autoform@5.8.1');
-  api.use('lepozepo:s3@5.2.4');
+  api.use('lepozepo:s3@5.2.4'); 
+  api.use('tmeasday:check-npm-versions');
   // api.addAssets(['addImageTemplate.html'], 'client');
   api.addFiles([
     'addImageTemplate.html',
   ], 'client');
   api.addFiles(['SlingshotConfig.js','S3Methods.js'],['client','server']);
   api.addFiles(['SlingshotDirective.js','S3Config.js'],['server']);
-
-  api.mainModule('autoform-images.js', 'client');
-
+  api.addFiles('autoform-images.js', 'client');
 });
 
 Package.onTest(function(api) {
   api.use('ecmascript');
-  api.use('tinytest');
+  api.use('dispatch:mocha');
   api.use('maxjohansen:autoform-images');
-  // api.mainModule('autoform-images-tests.js');
+  api.addFiles('autoform-images-tests.js');
 });
