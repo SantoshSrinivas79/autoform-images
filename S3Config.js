3be3a93bb9733b865d5170bcb22f90dda687e75f
macsj200 <maxjohansen@berkeley.edu>
2017-03-09 00:55:15 -0800

    allow env vars

--- S3Config.js
+++ S3Config.js
@@ -1,6 +1,6 @@
 S3.config = {
-  key: Meteor.settings.AWSAccessKeyId,
-  secret: Meteor.settings.AWSSecretAccessKey,
-  bucket: Meteor.settings.AWSBucket,
-  region: Meteor.settings.AWSRegion
+  key: Meteor.settings.AWSAccessKeyId || process.env.AWSAccessKeyId,
+  secret: Meteor.settings.AWSSecretAccessKey || process.env.AWSSecretAccessKey,
+  bucket: Meteor.settings.AWSBucket || process.env.AWSBucket,
+  region: Meteor.settings.AWSRegion || process.env.AWSRegion,
 };
