22d6311ca8e116fa76772df4c3d438237835f5ed
macsj200 <maxjohansen@berkeley.edu>
2017-07-14 13:22:58 -0700

    update croppie

--- autoform-images.js
+++ autoform-images.js
@@ -12,11 +12,11 @@ if(logging)
 
 log('we are logging');
 checkNpmVersions({
-    'croppie':'2.4.1',
+    'croppie':'2.5.0',
     'exif-js':'2.2.1',
 },'maxjohansen:autoform-images');
 require('croppie/croppie.css');
-exifJs = require('exif-js');
+EXIF = require('exif-js');
 Croppie = require('croppie');
 
 const fileUrlMap = {};
@@ -40,6 +40,7 @@ Template.addImageElemTemplate.onCreated(function(){
   }
   this.croppieExists = new ReactiveVar(false);
   this.croppieId = this.data.name.replace(".","-");
+  const croppieIdSave = this.croppieId;
   this.reader = new FileReader();
   this.reader.addEventListener("load", (event) => {
       this.uploader = null;
@@ -52,18 +53,47 @@ Template.addImageElemTemplate.onCreated(function(){
       var ctx = canvas.getContext('2d');
       var img = new Image();
       img.src = window.URL.createObjectURL(blob);
+      let exifInfo;
+      try {
+        exifInfo = EXIF.readFromBinaryFile(event.target.result);
+      } catch (e) {
+        // couldn't extract EXIF metadata
+      }
       const MAX_BYTES = 1000000;
       img.onload = () => {
+        // set proper canvas dimensions before transform & export
+        // if (exifInfo && [5,6,7,8].indexOf(exifInfo.Orientation) > -1) {
+        //   canvas.width = img.height;
+        //   canvas.height = img.width;
+        // } else {
+          // canvas.width = img.width;
+          // canvas.height = img.height;
+        // }
         canvas.width = img.width;
         canvas.height = img.height;
         if (blob.size > MAX_BYTES) {
           canvas.width = canvas.width / 4;
           canvas.height = canvas.height / 4;
         }
+        // if (exifInfo) {
+        //   switch (exifInfo.Orientation) {
+        //     case 2: ctx.transform(-1, 0, 0, 1, canvas.width, 0); break;
+        //     case 3: ctx.transform(-1, 0, 0, -1, canvas.width, canvas.height ); break;
+        //     case 4: ctx.transform(1, 0, 0, -1, 0, canvas.height ); break;
+        //     case 5: ctx.transform(0, 1, 1, 0, 0, 0); break;
+        //     case 6: ctx.transform(0, 1, -1, 0, canvas.height , 0); break;
+        //     case 7: ctx.transform(0, -1, -1, 0, canvas.height , canvas.width); break;
+        //     case 8: ctx.transform(0, -1, 1, 0, 0, canvas.width); break;
+        //     default: ctx.transform(1, 0, 0, 1, 0, 0);
+        //   }
+        // }
         ctx.drawImage(img,0,0,canvas.width,canvas.height);
-        croppieEl.bind({
+        let config = {
             url:canvas.toDataURL(),
-        });
+            orientation: exifInfo && exifInfo.Orientation,
+        };
+        croppieEl.bind(config);
+        // debugger;
       };
       this.croppieExists.set(true);
       log('new croppie bound');
@@ -100,7 +130,8 @@ Template.addImageElemTemplate.events({
         boundary: {
             width: '100%',
             height:300,
-        }
+        },
+        enableOrientation: true,
       });
       log('new croppie created');
       Template.instance().reader.readAsArrayBuffer(event.target.files[0]);
